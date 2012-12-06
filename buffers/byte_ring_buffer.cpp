ad227d5ca171b7b6851b54f52ec506fa69f20281
uozuAho <uozu.aho@gmail.com>
2012-12-06 16:55:54 +1100

    Updated byte_ring_buffer

--- buffers/byte_ring_buffer.cpp
+++ buffers/byte_ring_buffer.cpp
@@ -3,6 +3,8 @@
 
 using namespace ucToolbox;
 
+// Not bothering with this constructor - otherwise every function would need to check if
+// storage had been set
 //ByteRingBuffer::ByteRingBuffer() {
 //    storage = NULL;
 //    get_idx = put_idx = storage_capacity = 0;
@@ -31,6 +33,13 @@ bool ByteRingBuffer::isEmpty() {
         return false;
 }
 
+ByteRingBuffer::return_value ByteRingBuffer::peek() {
+    if (isEmpty())
+        return FAIL;
+    else
+        return static_cast<return_value>(storage[get_idx]);
+}
+
 ByteRingBuffer::return_value ByteRingBuffer::get() {
     if (isEmpty())
         return FAIL;
@@ -39,7 +48,7 @@ ByteRingBuffer::return_value ByteRingBuffer::get() {
         uint16_t temp = get_idx;
         if (++get_idx == storage_capacity)
             get_idx = 0;
-        return storage[temp];
+        return static_cast<return_value>(storage[temp]);
     }
 }
 
@@ -52,6 +61,29 @@ ByteRingBuffer::return_value ByteRingBuffer::put(int8_t byte) {
             put_idx = 0;
         if (put_idx == get_idx)
             full = true;
-        return storage[temp];
+        return OK;
+    }
+}
+
+ByteRingBuffer::return_value ByteRingBuffer::write(int8_t* buf, uint16_t len) {
+    if (space() < len)
+        return FAIL;
+    else {
+        int i = 0;
+        for(; i < len; i++) {
+            put(buf[i]);
+        }
+        return OK;
+    }
+}
+
+uint16_t ByteRingBuffer::space() {
+    if (full)
+        return 0;
+    else {
+        int32_t len = get_idx - put_idx;
+        if (len <= 0)
+            len += storage_capacity;
+        return static_cast<uint16_t>(len);
     }
 }
--- buffers/byte_ring_buffer.hpp
+++ buffers/byte_ring_buffer.hpp
@@ -21,6 +21,7 @@ public:
     return_value peek();
     return_value get();
     return_value put(int8_t byte);
+    return_value write(int8_t* buf, uint16_t len);
     bool isFull();
     bool isEmpty();
     void setStorage(int8_t* location, uint16_t capacity);
@@ -30,28 +31,10 @@ private:
     uint16_t storage_capacity;
     uint16_t get_idx, put_idx;
     bool full; // flag used since get_idx == put_idx could mean the buffer is empty or full
+
+    uint16_t space();
 };
 
 } // end namespace ucToolbox
-
-//typedef struct {
-//    uc* ucpBuffer;
-//    const us usBufSize;
-//    REG16 usPutIdx, usGetIdx;
-//} Ringbuf;
-//
-//bool RINGBUF_bIsFull(Ringbuf* ringbuf);
-//bool RINGBUF_bIsEmpty(Ringbuf* ringbuf);
-//us RINGBUF_usSpace(Ringbuf* ringbuf);
-//us RINGBUF_usBytesInStream(Ringbuf* buf);
-//ss RINGBUF_ssPeek(Ringbuf* ringbuf);
-//ss RINGBUF_ssGetByte(Ringbuf* ringbuf);
-//ss RINGBUF_ssPutByte(Ringbuf* ringbuf, uc byte);
-//us RINGBUF_ssWrite(Ringbuf* ringbuf, uc* input_buf, us len);
-//
-//// Debugging functions
-//void RINGBUF_vPrintToStdout(Ringbuf* ringbuf);
-//void RINGBUF_vPrintToStdout_detailed(Ringbuf* buf);
-//void RINGBUF_vUnitTest();
 
 #endif // #ifndef BYTE_RING_BUFFER_H
